1842c1412afa01d16245f64083c6bdb467960f49
DarkSekki <k@byx.ch>
2025-08-14 12:58:47 +0200

    fix pdf issue

--- build.sh
+++ build.sh
@@ -10,8 +10,12 @@ VARIABLE_FILE="${1:-letter_variable.md}"
 STATIC_FILE="${2:-letter_static.md}"
 OUTPUT="${3:-Motivationsschreiben.pdf}"
 
-cd "$(dirname "$0")"
-
-pandoc "$VARIABLE_FILE" "$STATIC_FILE"   --template=letter.tex   -V lang=de   -V papersize=a4   -V geometry:margin=2.5cm   -o "$OUTPUT"
+pandoc "$VARIABLE_FILE" "$STATIC_FILE" \
+  --template=/opt/template/letter.tex \
+  -V lang=de \
+  -V papersize=a4 \
+  -V geometry:margin=2.5cm \
+  -o "$OUTPUT"
 
 echo "Fertig: $OUTPUT"
+
